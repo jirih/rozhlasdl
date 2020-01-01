bb9961f639246a7f53053dee610682ee5553d55c
jirih <3437851+jirih@users.noreply.github.com>
2020-01-01 16:05:43 +0200

    Fixed command line argument

--- rozhlasdl/rozhlasdl.py
+++ rozhlasdl/rozhlasdl.py
@@ -17,8 +17,8 @@ def create_parser():
     parser.add_argument('url', nargs='+', help='<Required> URLs', action='store')
     parser.add_argument('-d', '--dir', help='Directory for saving downloaded files',
                         default=join(str(Path.home()), "Downloads"), action='store')
-    parser.add_argument('-n', '--no-duplicates', help='No duplicates are kept',
-                        dest="no_duplicates", default=True, action='store_true')
+    parser.add_argument('-n', '--no-duplicate-skipping', help='Duplicates are not skipped',
+                        dest="no_duplicate_skipping", default=False, action='store_true')
     return parser
 
 
@@ -26,7 +26,7 @@ def main():
     parser = create_parser()
     urls = parser.parse_args().url
     folder = parser.parse_args().dir
-    no_duplicates = parser.parse_args().no_duplicates
+    no_duplicates = not parser.parse_args().no_duplicate_skipping
 
     if not isabs(folder):
         folder = join(str(Path.home()), "Downloads", folder)
